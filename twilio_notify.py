07834e48944956f41ed22efa167a1f2d167886dc
Musya Joseph <57936020+JosephMusya@users.noreply.github.com>
2022-05-13 17:24:52 +0300

    Update twilio_notify.py

--- twilio_notify.py
+++ twilio_notify.py
@@ -1,15 +1,14 @@
 from twilio.rest import Client
-sid = 'AC0edb7483f121df854911129599126e12'
-token = 'ac13403f408a3035e790bd955160e0c4'
+sid = ''
+token = ''
 def notify(message):
     try:
         client = Client(sid,token)
-        client.messages.create(messaging_service_sid = 'MG0d44854d547ca3459c52ab02509f5386',
-                               #from_ = 'Poultra',#['+17194972524'],
+        client.messages.create(messaging_service_sid = '',
                                body = message,
-                               to = ['+254726493355'])
+                               to = [''])
     except Exception as e:
         print(e)
         pass
 if __name__ == '__main__':
-    notify('Helooo')
+    notify('')
